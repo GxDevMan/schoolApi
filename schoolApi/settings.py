4f4192df29f66fa6037ec2c3e2d29f26ddfa6a07
Feivel Doctora <feivellixen.doctora.iics@ust.edu.ph>
2023-02-14 16:34:00 +0800

    - removed custom hash

--- schoolApi/settings.py
+++ schoolApi/settings.py
@@ -95,7 +95,6 @@ DATABASES = {
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'userPortal.hashers.customHash',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
